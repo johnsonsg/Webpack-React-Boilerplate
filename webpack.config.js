f1b394c0ca87c021f3f3a1940c5b22555d24a0a9
Shawn Johnson <johnson.sg@gmail.com>
2023-01-17 08:55:06 -0500

    reconfig webpackconfig, added plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,34 @@
-const path = require("path");
-
+//webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  mode: 'development',
-  devServer: {
-    static: path.resolve(__dirname, "./src"),
-    historyApiFallback: true,
-  },
-  entry: path.resolve(__dirname, "./src/index.js"),
-  module: {
-    rules: [
-      {
-        // test: /\.jsx$/,
-        test: /\.(jsx|js)$/,
-        exclude: /node_modules/,
-        use: "babel-loader"
-      }
-    ]
-  },
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: "bundle.js"
-  }
-};
+   entry: path.resolve(__dirname, "./src/index.js"),
+   output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'bundle.js'
+   },
+   mode: 'development',
+   devServer: {
+      static: path.resolve(__dirname, "./src"),
+      historyApiFallback: true,
+      port: 8080
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(jsx|js)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+         },
+         {
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }
+      ]
+   },
+   plugins:[
+       new HtmlWebpackPlugin({
+            template: path.join(__dirname,'/src/index.html')
+       })
+   ]
+}
